523a96d7dec2deddcb8f78d270acc121d37475ab
Dair Aidarkhanov <dairaidarkhanov@gmail.com>
2022-07-15 22:22:46 +0600

    Add necessary modules from Solid.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import type { Component } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
+  const [seats, setSeats] = createSignal(0);
+
   return (
     <For each={}>{(seat, index) =>
       {index() + 1}: <div class="seat"></div>
